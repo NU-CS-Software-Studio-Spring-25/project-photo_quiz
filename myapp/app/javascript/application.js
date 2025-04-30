75976ef42d401acd3e9b28412e2ed5c3a1452e58
syshin402 <syshin4072@gmail.com>
2025-04-30 15:28:09 -0500

    fixing delete..

--- myapp/app/javascript/application.js
+++ myapp/app/javascript/application.js
@@ -4,5 +4,9 @@ import "controllers"
 import "bootstrap"
 import "@popperjs/core" // Required for Bootstrap tooltips and popovers
 import "@rails/ujs";
-
+import Rails from "@rails/ujs"
 Rails.start();
+
+import "@hotwired/turbo-rails"   // if you’re using Turbo
+import "./controllers"           // if you have Stimulus
+
