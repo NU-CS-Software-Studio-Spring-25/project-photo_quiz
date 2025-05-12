d1fe934f152200efb55ab3456e723cb069cb193b
Pan Yuyang <panyuyang@PandeMacBook-Pro.local>
2025-05-11 23:23:41 -0700

    Skip CSRF token check for API requests

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
--- myapp/app/controllers/students_controller.rb
+++ myapp/app/controllers/students_controller.rb
@@ -1,78 +1,70 @@
 class StudentsController < ApplicationController
-    before_action :set_student, only: [ :show, :edit, :update, :destroy, :confirm_destroy ]
+  before_action :set_student, only: [ :show, :update, :destroy ]
 
-    # GET /students
-    # GET /students.json
-    def index
-      @student = Student.all
+  # GET /students
+  # GET /students.json
+  def index
+    @students = Student.all.with_attached_profile_picture
+    render json: @students.map { |s| student_json(s) }
+  end
+
+  # GET /students/1
+  # GET /students/1.json
+  def show
+    render json: student_json(@student)
+  end
+
+  # POST /students
+  # POST /students.json
+  def create
+    @student = Student.new(student_params)
+
+    # Attach profile picture if provided
+    if params[:profile_picture]
+      @student.profile_picture.attach(params[:profile_picture])
     end
 
-    # GET /students/1
-    # GET /students/1.json
-    def show
+    if @student.save
+      render json: student_json(@student), status: :created
+    else
+      render json: @student.errors, status: :unprocessable_entity
     end
+  end
 
-    # GET /students/new
-    def new
-      @student = Student.new
-    end
-
-    # GET /students/1/edit
-    def edit
-    end
-
-    # POST /students
-    # POST /students.json
-    def create
-      @student = Student.new(student_params)
-
-      respond_to do |format|
-        if @student.save
-          format.html { redirect_to students_path, notice: "Student was successfully created." }
-          format.json { render :show, status: :created, location: @student }
-        else
-          format.html { render :new }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /students/1
+  # PATCH/PUT /students/1.json
+  def update
+    if @student.update(student_params)
+      if params[:profile_picture]
+        @student.profile_picture.attach(params[:profile_picture])
       end
+      render json: student_json(@student)
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /students/1
+  # DELETE /students/1.json
+  def destroy
+    @student.destroy
+    head :no_content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_student
+      @student = Student.find(params[:id])
     end
 
-    # PATCH/PUT /students/1
-    # PATCH/PUT /students/1.json
-    def update
-      respond_to do |format|
-        if @student.update(student_params)
-          format.html { redirect_to students_path, notice: "Student was successfully updated." }
-          format.json { render :show, status: :ok, location: @student }
-        else
-          format.html { render :edit }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
-      end
+    # Only allow a list of trusted parameters through.
+    def student_params
+      params.permit(:first_name, :last_name, :name_mastery, :learned)
     end
 
-    # DELETE /students/1
-    # DELETE /students/1.json
-    def destroy
-      respond_to do |format|
-        if @student.destroy
-          format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
-          format.json { head :no_content }
-        else
-          format.html { redirect_to students_url, alert: "Failed to destroy student." }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
-      end
+    def student_json(student)
+      student.as_json.merge({
+        profile_picture_url: student.profile_picture.attached? ? url_for(student.profile_picture) : nil
+      })
     end
-
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_student
-        @student = Student.find(params[:id])
-      end
-
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def student_params
-        params.require(:student).permit(:first_name, :last_name, :course, :profile_picture)
-      end
 end
