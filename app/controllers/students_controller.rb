7a108a1af02819070c67d0b01fd85caa79277274
Pan Yuyang <panyuyang@PandeMacBook-Pro.local>
2025-05-12 00:41:07 -0700

    support JSON responses

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,79 +1,96 @@
+# app/controllers/students_controller.rb
 class StudentsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
-    before_action :set_student, only: [ :show, :edit, :update, :destroy, :confirm_destroy ]
+  respond_to :html, :json                                  
+  skip_before_action :verify_authenticity_token,           
+                       only: %i[create update destroy]
 
-    # GET /students
-    # GET /students.json
-    def index
-      @student = Student.all
+  before_action :set_student,                            \
+                only: %i[show edit update destroy confirm_destroy]
+
+  # GET /students
+  # GET /students.json
+  def index
+    @students = Student.all                                
+
+    respond_to do |format|
+      format.html                                          
+      format.json { render json: @students }             
     end
+  end
 
-    # GET /students/1
-    # GET /students/1.json
-    def show
+  # GET /students/1
+  # GET /students/1.json
+  def show
+    respond_to do |format|
+      format.html                                           
+      format.json { render json: @student }                 
     end
+  end
 
-    # GET /students/new
-    def new
-      @student = Student.new
-    end
+  # GET /students/new
+  def new
+    @student = Student.new
+  end
 
-    # GET /students/1/edit
-    def edit
-    end
+  # GET /students/1/edit
+  def edit; end
 
-    # POST /students
-    # POST /students.json
-    def create
-      @student = Student.new(student_params)
+  # POST /students
+  # POST /students.json
+  def create
+    @student = Student.new(student_params)
 
-      respond_to do |format|
-        if @student.save
-          format.html { redirect_to students_path, notice: "Student was successfully created." }
-          format.json { render :show, status: :created, location: @student }
-        else
-          format.html { render :new }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to students_path, notice: "Student was successfully created." }
+        format.json { render :show, status: :created, location: @student }
+      else
+        format.html { render :new }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    # PATCH/PUT /students/1
-    # PATCH/PUT /students/1.json
-    def update
-      respond_to do |format|
-        if @student.update(student_params)
-          format.html { redirect_to students_path, notice: "Student was successfully updated." }
-          format.json { render :show, status: :ok, location: @student }
-        else
-          format.html { render :edit }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /students/1
+  # PATCH/PUT /students/1.json
+  def update
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to students_path, notice: "Student was successfully updated." }
+        format.json { render :show, status: :ok, location: @student }
+      else
+        format.html { render :edit }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    # DELETE /students/1
-    # DELETE /students/1.json
-    def destroy
-      respond_to do |format|
-        if @student.destroy
-          format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
-          format.json { head :no_content }
-        else
-          format.html { redirect_to students_url, alert: "Failed to destroy student." }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+  # DELETE /students/1
+  # DELETE /students/1.json
+  def destroy
+    respond_to do |format|
+      if @student.destroy
+        format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to students_url, alert: "Failed to destroy student." }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_student
-        @student = Student.find(params[:id])
-      end
+  private
 
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def student_params
-        params.require(:student).permit(:first_name, :last_name, :course, :profile_picture, :name_mastery, :learned)
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def student_params
+      params.require(:student).permit(
+        :first_name, :last_name, :course, :profile_picture,
+        :name_mastery, :learned
+      )
+    end
 end
