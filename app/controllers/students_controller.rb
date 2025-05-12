2183c85d8a397f16475b66beab4f46ac1b552f10
Pan Yuyang <panyuyang@PandeMacBook-Pro.local>
2025-05-11 23:55:19 -0700

    Permit name_mastery and learned in student_params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -73,6 +73,6 @@ class StudentsController < ApplicationController
 
       # Never trust parameters from the scary internet, only allow the white list through.
       def student_params
-        params.require(:student).permit(:first_name, :last_name, :course, :profile_picture)
+        params.require(:student).permit(:first_name, :last_name, :course, :profile_picture, :name_mastery, :learned)
       end
 end
