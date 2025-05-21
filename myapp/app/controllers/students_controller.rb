7f1eab38f7e7db773fd5a81a1067e963dd0a6a08
pan1018 <pan.1018@osu.edu>
2025-05-21 15:06:38 -0500

    Add student photo upload and display functionality

--- myapp/app/controllers/students_controller.rb
+++ myapp/app/controllers/students_controller.rb
@@ -1,86 +1,103 @@
 class StudentsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_student, only: [ :show, :edit, :update, :destroy, :confirm_destroy ]
-    before_action :load_courses,  only: %i[ new edit create update ]
-    # GET /students
-    # GET /students.json
-    def index
-      @courses = Course.includes(:students).all
-      @students = Student.all
-    end
+  before_action :authenticate_user!
+  before_action :set_student, only: [ :show, :edit, :update, :destroy, :confirm_destroy ]
+  before_action :load_courses,  only: %i[ new edit create update ]
 
-    # GET /students/1
-    # GET /students/1.json
-    def show
-    end
+  # GET /students
+  # GET /students.json
+  def index
+    @courses = Course.includes(:students).all
+    @students = Student.all
+  end
 
-    # GET /students/new
-    def new
-      @student = Student.new
-    end
+  # GET /students/1
+  # GET /students/1.json
+  def show
+  end
 
-    # GET /students/1/edit
-    def edit
-    end
+  # GET /students/new
+  def new
+    @student = Student.new
+  end
 
-    # POST /students
-    # POST /students.json
-    def create
-      @student = Student.new(student_params)
+  # GET /students/1/edit
+  def edit
+  end
 
-      respond_to do |format|
-        if @student.save
-          course = Course.find(params[:course_id])
-          @student.update(course: course.name)
-          Membership.create!(student: @student, course: course, user: current_user)
-          format.html { redirect_to students_path, notice: "Student was successfully created." }
-          format.json { render :show, status: :created, location: @student }
+  # POST /students
+  # POST /students.json
+  def create
+    @student = Student.new(student_params)
+  
+    respond_to do |format|
+      if @student.save
+        if params[:course_id].present?
+          begin
+            course = Course.find(params[:course_id])
+            @student.update(course: course.name) # 你原有的逻辑保留
+            Membership.create!(student: @student, course: course, user: current_user)
+  
+            format.html { redirect_to students_path, notice: "Student was successfully created." }
+            format.json { render :show, status: :created, location: @student }
+          rescue ActiveRecord::RecordNotFound
+            format.html { redirect_to new_student_path, alert: "Selected course not found." }
+            format.json { render json: { error: "Course not found" }, status: :unprocessable_entity }
+          end
         else
-          format.html { render :new }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
+          format.html { redirect_to new_student_path, alert: "Please select a course." }
+          format.json { render json: { error: "No course selected" }, status: :unprocessable_entity }
         end
+      else
+        format.html { render :new }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
+  
 
-    # PATCH/PUT /students/1
-    # PATCH/PUT /students/1.json
-    def update
-      respond_to do |format|
-        if @student.update(student_params)
-          format.html { redirect_to students_path, notice: "Student was successfully updated." }
-          format.json { render :show, status: :ok, location: @student }
-        else
-          format.html { render :edit }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /students/1
+  # PATCH/PUT /students/1.json
+  def update
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to students_path, notice: "Student was successfully updated." }
+        format.json { render :show, status: :ok, location: @student }
+      else
+        format.html { render :edit }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    # DELETE /students/1
-    # DELETE /students/1.json
-    def destroy
-      respond_to do |format|
-        if @student.destroy
-          format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
-          format.json { head :no_content }
-        else
-          format.html { redirect_to students_url, alert: "Failed to destroy student." }
-          format.json { render json: @student.errors, status: :unprocessable_entity }
-        end
+  # DELETE /students/1
+  # DELETE /students/1.json
+  def destroy
+    respond_to do |format|
+      if @student.destroy
+        format.html { redirect_to students_path, notice: "Student was successfully destroyed." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to students_url, alert: "Failed to destroy student." }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    private
-      def load_courses
-        @courses = Student.distinct.pluck(:course)
-      end
-      # Use callbacks to share common setup or constraints between actions.
-      def set_student
-        @student = Student.find(params[:id])
-      end
+  private
 
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def student_params
-        params.require(:student).permit(:first_name, :last_name, :profile_picture, :course)
-      end
+    def load_courses
+      @courses = Student.distinct.pluck(:course)
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def student_params
+      params.require(:student).permit(:first_name, :last_name, :profile_picture)
+    end
+    
+    
 end
