43fad30fb2694dc9f7ca058f4fa82ca2a4a8884a
PaulaFregene <paulafregene@gmail.com>
2025-05-24 21:56:31 -0500

    fix: regrex validations for inputs for student and course

--- myapp/app/models/course.rb
+++ myapp/app/models/course.rb
@@ -4,4 +4,6 @@ class Course < ApplicationRecord
   has_many :students, through: :memberships
   has_many :users, through: :memberships
   validates :name, presence: true
+qui  VALID_COURSE_NAME_REGEX = /\A[a-zA-Z0-9\s\-\']+\z/
+  validates :name, format: { with: VALID_COURSE_NAME_REGEX, message: "only allows letters, numbers, spaces, hyphens, and apostrophes" }
 end
--- myapp/app/models/student.rb
+++ myapp/app/models/student.rb
@@ -2,6 +2,9 @@ class Student < ActiveRecord::Base
     has_many :memberships, dependent: :destroy
     has_many :courses, through: :memberships
     accepts_nested_attributes_for :memberships
-    validates :first_name, :last_name, :course, presence: true
+    validates :first_name, :last_name, presence: true
+    VALID_NAME_REGEX = /\A[a-zA-Z\s\-']+\z/
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: "only allows letters, spaces, hyphens, and apostrophes" }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: "only allows letters, spaces, hyphens, and apostrophes" }
     has_one_attached :profile_picture
 end
