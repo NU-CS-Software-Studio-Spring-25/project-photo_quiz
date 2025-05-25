bdb3505b3d6bdc84ce54b779c72e1c1405399930
PaulaFregene <paulafregene@gmail.com>
2025-05-25 16:58:02 -0500

    fix: javascript controller connect for searching and quiz

--- myapp/app/javascript/controllers/application.js
+++ myapp/app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import SearchController from "controllers/search_controller"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
+application.register("search", SearchController)
+
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
