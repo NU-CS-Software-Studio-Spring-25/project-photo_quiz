6bc5762d985afa53a752b4d8cd41a1ef085417f1
Seungyeon Shin <157871377+syshin402@users.noreply.github.com>
2025-05-26 01:20:53 -0500

    Revert "Flash CSS styling & Error pages added"

--- app/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-  # Custom error handling
-  def render_error(status)
-    render template: "errors/#{status}", status: status, layout: "application"
-  end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-  # Custom error pages
-  %w(404 422 500).each do |code|
-    get code, to: "errors#show", code: code
-  end
