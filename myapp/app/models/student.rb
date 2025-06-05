87227f188ca09e9fa1276ea08968c9c970848c65
PaulaFregene <paulafregene@gmail.com>
2025-06-04 21:21:11 -0500

    accessibility for students labels done

--- myapp/app/models/student.rb
+++ myapp/app/models/student.rb
@@ -7,4 +7,15 @@ class Student < ActiveRecord::Base
     validates :first_name, format: { with: VALID_NAME_REGEX, message: "only allows letters, spaces, hyphens, and apostrophes" }
     validates :last_name, format: { with: VALID_NAME_REGEX, message: "only allows letters, spaces, hyphens, and apostrophes" }
     has_one_attached :profile_picture
+    validate :profile_picture_size
+
+    def profile_picture_size
+        if profile_picture.attached? && profile_picture.blob.byte_size > 5.megabytes
+            errors.add(:profile_picture, "must be less than 5MB")
+        end
+    end
+
+
+
+
 end
